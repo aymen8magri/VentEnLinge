3fecf3599926d8eda5de599b01a0e557d2f924e1
aymenMagri <aymen00magri@gmail.com>
2024-10-14 22:34:08 +0100

    navBar & Footer done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './compnents/home/home.component';
+import { AboutUsComponent } from './compnents/about-us/about-us.component';
+import { ContactUsComponent } from './compnents/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'home',title:'Home',component:HomeComponent},
+    {path:'aboutUs',title:'About Us',component:AboutUsComponent},
+    {path:'contactUs',title:'Contact Us',component:ContactUsComponent}
+];
