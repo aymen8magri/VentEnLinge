7c812d96fdb27b12cdc5b0e78cfd0581e8a12219
aymenMagri <aymen00magri@gmail.com>
2024-10-15 11:00:20 +0100

    404Error 1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './compnents/home/home.component';
 import { AboutUsComponent } from './compnents/about-us/about-us.component';
 import { ContactUsComponent } from './compnents/contact-us/contact-us.component';
+import { ErrorComponent } from './compnents/error/error.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'home',title:'Home',component:HomeComponent},
     {path:'aboutUs',title:'About Us',component:AboutUsComponent},
-    {path:'contactUs',title:'Contact Us',component:ContactUsComponent}
+    {path:'contactUs',title:'Contact Us',component:ContactUsComponent},
+    {path:'**',title:'404',component:ErrorComponent}
 ];
