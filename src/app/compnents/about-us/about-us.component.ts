5245a9621c577e5fa853575afc58eaf55aeccd24
aymenMagri <aymen00magri@gmail.com>
2024-10-15 20:32:51 +0100

    edit links

--- src/app/compnents/about-us/about-us.component.ts
+++ src/app/compnents/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
+  router: Router = inject(Router);
+
+  onContactUs() {
+    this.router.navigate(['/contactUs']);
+  }
 
 }
